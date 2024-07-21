b7d18168890262786c3f165e66736ee950b3830b
Jan Balangue <jan.a.balangue@gmail.com>
2024-07-20 17:43:25 -0700

    redo project

---
+++ src/features/campsites/CampsitesList.js
@@ -0,0 +1,17 @@
+import { CAMPSITES } from "../../app/shared/CAMPSITES";
+import CampsiteCard from "./CampsiteCard";
+import { Col, Row } from "reactstrap";
+
+const CampsitesList = () => {
+  return (
+    <Row className="ms-auto">
+      {CAMPSITES.map((campsite) => (
+        <Col md="5" className="m-4" key={campsite.id}>
+          <CampsiteCard campsite={campsite} />
+        </Col>
+      ))}
+    </Row>
+  );
+};
+
+export default CampsitesList;
