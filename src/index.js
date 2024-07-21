54b88eb8a2fc78f9501970144dbe0272911ef34f
Jan Balangue <jan.a.balangue@gmail.com>
2024-07-20 17:56:13 -0700

    Merge branch 'main' of https://github.com/rockleejb/nucampsite-react

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -12,21 +11,6 @@ import 'typeface-lobster';
 import 'typeface-open-sans';
 
 const container = document.getElementById('root');
-=======
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-import App from "./App";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-social/bootstrap-social.css";
-import "font-awesome/css/font-awesome.css";
-import "typeface-lobster";
-import "typeface-open-sans";
-
-const container = document.getElementById("root");
->>>>>>> bf6e0ee988a6053270cd10db428b3e04b40ec769
 const root = createRoot(container);
 
 root.render(
@@ -36,7 +20,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-<<<<<<< HEAD
-
-=======
->>>>>>> bf6e0ee988a6053270cd10db428b3e04b40ec769
