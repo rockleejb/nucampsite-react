9766a2daf343c9e6e98b4ef671a513b5ec044bf0
Jan Balangue <jan.a.balangue@gmail.com>
2024-07-25 16:54:41 -0700

    Comments

--- src/features/campsites/CampsiteDetail.js
+++ src/features/campsites/CampsiteDetail.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardText, CardBody, Col } from "reactstrap";
 const CampsiteDetail = ({ campsite }) => {
   const { id, name, image, elevation, featured, description } = campsite;
   return (
-    <Col md="12" className="m-4" key={id}>
+    <Col md="5" className="m-1" key={id}>
       <Card>
         <CardImg src={image} alt={name} />
       </Card>
