c11dc35f445a8a819e5df0321f54dcec84cd68b5
Jan Balangue <jan.a.balangue@gmail.com>
2024-07-27 12:34:53 -0700

    Week 2

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,10 +1,15 @@
 import { Row, Col } from "reactstrap";
 import { selectFeaturedCampsite } from "../campsites/campsitesSlice";
 import { selectFeaturedPromotion } from "../promotions/promotionsSlice";
+import { selectFeaturedPartner } from "../partners/partnersSlice";
 import DisplayCard from "./DisplayCard";
 
 const DisplayList = () => {
-  const items = [selectFeaturedCampsite(), selectFeaturedPromotion()];
+  const items = [
+    selectFeaturedCampsite(),
+    selectFeaturedPromotion(),
+    selectFeaturedPartner(),
+  ];
   return (
     <Row>
       {items.map((item, idx) => (
